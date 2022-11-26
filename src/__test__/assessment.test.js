11202833428ca09c3103bd8bb261a5bb9df57b47
Delimaanindi <delima2730@gmail.com>
2022-11-26 16:35:45 +0700

    adding a word in test desc

--- src/__test__/assessment.test.js
+++ src/__test__/assessment.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import sockMerchant from "./socksales";
 
-test('shall give the number of pair of socks', () => {
+test('shall give the correct number of pair of socks', () => {
     expect(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20])).toBe(3);
   });
